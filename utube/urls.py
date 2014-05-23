f17bab7382bf000f718c166f9d158ac556ed4d13
BeshoSmile <beshosmile@gmail.com>
2014-05-23 13:18:57 +0200

    Add path call for api call testing

--- utube/urls.py
+++ utube/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-
 from django.contrib import admin
 
 
@@ -10,6 +9,8 @@ urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
+    url(r"^call/",'mainapp.views.call' ),
+
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
