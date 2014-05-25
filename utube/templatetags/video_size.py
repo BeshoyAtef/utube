38fa7f55f656c7d293f685f160896f0fac8ddda2
BeshoSmile <beshosmile@gmail.com>
2014-05-25 08:57:23 +0200

    Improve results display

--- utube/templatetags/video_size.py
+++ utube/templatetags/video_size.py
@@ -1,10 +1,26 @@
 from django import template
 import requests
-
+from django.template.defaultfilters import filesizeformat
 register = template.Library()
 
 @register.simple_tag
 def get_size(url):
     vid = requests.head(url)
-    size = vid.headers['content-length']
-    return size
+    size = vid.headers['Content-Length']
+    return filesizeformat(int(size))
+
+@register.simple_tag
+def get_name(str_name):
+    name_array=str_name.split('x')
+    try:
+        name=name_array[1]
+    except:
+        name_array=str_name.split(' ')
+        try:
+            name = name_array[2]
+            formated = name_array[3]
+            return name + '<span class="label label-danger">'+str(formated)+'</span>'
+        except:
+            name = str_name
+            return name
+    return name+str('p')
