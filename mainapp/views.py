35116f65925780a765edfa5e0600d7adbd15b8d7
BeshoSmile <beshosmile@gmail.com>
2014-05-29 10:23:11 +0200

    Add Ajax call and response

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,16 +7,18 @@ import json
 
 # Create your views here.
 def call(request):
-	url = "	https://youtube-dl.appspot.com/api/info?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DnDS0Sr2Fhfk"
+	print (request)
+	link = request.POST.get('link')
+	url = "https://youtube-dl.appspot.com/api/info?url="+link
 	try:
 	  result = urllib2.urlopen(url)
 	  data = json.load(result)
-	  return render_to_response('homepage.html', {
+	  return render_to_response('api.html', {
 	  	'apicall':data['videos'][0]['formats'],
 	  	'vidtitle' : data['videos'][0]['title'].replace (" ", "_")[:200],
 	  	})
 	except urllib2.URLError, e:
-	  handleError(e)
+	  print(e)
 	  return render_to_response('homepage.html')
 
 # adata['videos'][0]['formats'][18]['format']
